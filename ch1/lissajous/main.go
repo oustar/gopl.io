3828aa4af34d111ad578e76e25a5fba88756e106
oustar <wenxingou@126.com>
2019-07-26 16:22:46 +0800

    完成第一章内容

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -16,6 +16,7 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"strconv"
 )
 
 //!-main
@@ -45,22 +46,30 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	if len(os.Args) > 1 && os.Args[1] == "web" {
-		//!+http
-		handler := func(w http.ResponseWriter, r *http.Request) {
-			lissajous(w)
-		}
+
 		http.HandleFunc("/", handler)
 		//!-http
 		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 		return
 	}
 	//!+main
-	lissajous(os.Stdout)
+	lissajous(os.Stdout, 5)
 }
 
-func lissajous(out io.Writer) {
+//!+http
+func handler(w http.ResponseWriter, r *http.Request) {
+	strcycles := r.URL.Query().Get("cycles")
+	cycles, err := strconv.Atoi(strcycles)
+	if err != nil {
+		cycles = 5
+	}
+	//fmt.Fprintf(w, "Cycles = %d\n", cycles)
+	lissajous(w, cycles)
+
+}
+func lissajous(out io.Writer, cycles int) {
 	const (
-		cycles  = 5     // number of complete x oscillator revolutions
+		//cycles  = 5     // number of complete x oscillator revolutions
 		res     = 0.001 // angular resolution
 		size    = 100   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
@@ -72,7 +81,7 @@ func lissajous(out io.Writer) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
